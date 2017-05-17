df1c24137d00203a542a8f8acb7baf671fd7c07b
Deadspeck <thaone344@yahoo.com>
2017-05-17 12:07:40 -0600

    instantiated run and converter

--- src/finalprac/converter.java
+++ src/finalprac/converter.java
@@ -1,5 +1,5 @@
 package finalprac;
 
 public class converter {
-
+	
 }
--- src/finalprac/run.java
+++ src/finalprac/run.java
@@ -4,7 +4,9 @@ public class run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		run myrun = new run();
+		converter con = new converter();
+		
 	}
 
 }
