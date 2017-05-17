fdeb211f9db10d57b732eed5cb6d58078ed8e94a
Deadspeck <thaone344@yahoo.com>
2017-05-17 12:11:49 -0600

    intro message added

--- src/finalprac/run.java
+++ src/finalprac/run.java
@@ -1,12 +1,14 @@
 package finalprac;
 
+import javax.swing.JOptionPane;
+
 public class run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		run myrun = new run();
 		converter con = new converter();
-		
+		JOptionPane.showMessageDialog(null, "Used For Converting X to Y");
 	}
 
 }
